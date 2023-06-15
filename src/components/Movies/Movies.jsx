4d6a18f4a47fc37c22aec3a2e28d785aab708945
smiadv2022 <msorokina@spacemedia.com.ua>
2023-06-15 16:02:22 +0300

    Update Movies.jsx

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -2,35 +2,35 @@ import { getMoviesSearch } from 'components/Api/Api';
 import { Container } from 'components/App.styled';
 import { SearchbarForm } from 'components/SearchBar/SearchBar';
 import { TrandingMoviesList } from 'components/TrandigList/TrandingList';
-import {
-  Title,
-  TrandigList,
-  TrandigListItem,
-} from 'components/TrandigList/TrandingList.styled';
+// import {
+//   Title,
+//   TrandigList,
+//   TrandigListItem,
+// } from 'components/TrandigList/TrandingList.styled';
 // import { SearchForm } from 'components/SearchBar/SearchBar.styled';
 import React, { useEffect } from 'react';
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 // import { useSearchParams } from 'react-router-dom';
 
 export const Movies = () => {
   // const query = useSearchParams.get('query');
   const [search, setSearch] = useState('');
-  const [page, setPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(0);
+  // const [page, setPage] = useState(1);
+  // const [totalPages, setTotalPages] = useState(0);
   const [images, setImages] = useState([]);
 
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
+  // const [error, setError] = useState(null);
+  // const [isLoading, setIsLoading] = useState(false);
 
   const handleSearch = searchText => {
     // console.log(searchText, 'sbar', search);
     setSearch(searchText);
     setImages([]);
-    setError(null);
-    setIsLoading(true);
-    setPage(1);
-    setTotalPages(0);
+    // setError(null);
+    // setIsLoading(true);
+    // setPage(1);
+    // setTotalPages(0);
     // setImages([]);
 
     console.log(searchText, 'sss', search);
